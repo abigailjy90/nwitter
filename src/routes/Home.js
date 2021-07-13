3ce95a8d887a8cf5ba09d3c4108c05cc24cdb7cc
Jiyoung Moon <jiyoungmoon@Jiyoungui-MacBookPro.local>
2021-07-13 15:47:10 +0900

    05 edit profile/update profile

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,7 +8,7 @@ const Home = ({ userObj }) => {
     console.log(userObj);
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
-    const [attachment, setAttachment] = useState();
+    const [attachment, setAttachment] = useState("");
 
     useEffect(() => {
         dbService.collection("nweets").onSnapshot(snapshot => {
